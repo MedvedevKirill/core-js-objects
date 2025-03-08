b1d8415b604bfbaae1a7030803ae7a12f37906f2
Medvedev Kirill <medvedev_k@icloud.com>
2025-03-08 23:03:26 +0400

    feat: implement css selector builder

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -373,33 +373,132 @@ function group(array, keySelector, valueSelector) {
  *  For more examples see unit tests.
  */
 
+class Selector {
+  #parts = [];
+
+  #lastSelector = '';
+
+  element(value) {
+    if (this.#lastSelector === 'element') {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector'
+      );
+    }
+    if (this.#lastSelector !== '') {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'
+      );
+    }
+    this.#parts.push(value);
+    this.#lastSelector = 'element';
+    return this;
+  }
+
+  id(value) {
+    if (this.#lastSelector === 'id') {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector'
+      );
+    }
+    if (this.#lastSelector !== 'element' && this.#lastSelector !== '') {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'
+      );
+    }
+    this.#parts.push(`#${value}`);
+    this.#lastSelector = 'id';
+    return this;
+  }
+
+  class(value) {
+    if (
+      this.#lastSelector === 'attribute' ||
+      this.#lastSelector === 'pseudoClass' ||
+      this.#lastSelector === 'pseudoElement'
+    ) {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'
+      );
+    }
+    this.#parts.push(`.${value}`);
+    this.#lastSelector = 'class';
+    return this;
+  }
+
+  attr(value) {
+    if (
+      this.#lastSelector === 'pseudoClass' ||
+      this.#lastSelector === 'pseudoElement'
+    ) {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'
+      );
+    }
+    this.#parts.push(`[${value}]`);
+    this.#lastSelector = 'attribute';
+    return this;
+  }
+
+  pseudoClass(value) {
+    if (this.#lastSelector === 'pseudoElement') {
+      throw new Error(
+        'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element'
+      );
+    }
+    this.#parts.push(`:${value}`);
+    this.#lastSelector = 'pseudoClass';
+    return this;
+  }
+
+  pseudoElement(value) {
+    if (this.#lastSelector === 'pseudoElement') {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector'
+      );
+    }
+    this.#parts.push(`::${value}`);
+    this.#lastSelector = 'pseudoElement';
+    return this;
+  }
+
+  combine(combinator, selector) {
+    this.#parts = this.#parts.concat(`${combinator}`, selector.#parts);
+    this.#lastSelector = selector.#lastSelector;
+    return this;
+  }
+
+  stringify() {
+    return this.#parts.join('');
+  }
+}
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    return new Selector().element(value);
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    return new Selector().id(value);
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    return new Selector().class(value);
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    return new Selector().attr(value);
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    return new Selector().pseudoClass(value);
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    return new Selector().pseudoElement(value);
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    return selector1.combine(` ${combinator} `, selector2);
   },
 };
 
